34d864dc150b8188db21fc74fddfb783773a774b
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-10-17 13:30:41 +0530

    - added sign in method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from "../form-input/form-input.component";
 import './sign-in.styles.scss'
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoogle} from "../../firebase/firebase.utils";
+import {auth, signInWithGoogle} from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
 
@@ -15,14 +15,22 @@ class SignIn extends React.Component {
         this.handleChange = this.handleChange.bind(this);
     };
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState(
-            {
-                email: "",
-                password: ""
-            }
-        )
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+
+            this.setState(
+                {
+                    email: "",
+                    password: ""
+                }
+            )
+        } catch (e) {
+            console.log(e.message);
+        }
+
     }
 
     handleChange = event => {
