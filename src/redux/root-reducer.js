b75927721876a56e97e20a46377004273ed823be
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-10-25 14:25:02 +0530

    - added directory redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,12 +5,14 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
     'key': 'root', storage, whitelist: ['cart']
 }
 const rootReducer = combineReducers({
     user: userReducer,
+    directory: directoryReducer,
     cart: cartReducer
 });
 
