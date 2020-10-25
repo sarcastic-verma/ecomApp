1fa68a125b9ce2b2c0ea284c132d1c4d0bea2871
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-10-25 16:09:27 +0530

    - added shop to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
-
 import CollectionPreview from '../../components/collection-preview/collection-preview';
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
+import {selectShopCollections} from "../../redux/shop/shop.selector";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
+const ShopPage = ({collections}) => {
     return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
+        <div className='shop-page'>
+            {collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+        </div>
     );
-  }
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector(
+    {collections: selectShopCollections}
+);
+
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,16 +4,19 @@ import {persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import userReducer from './user/user.reducer';
+import shopReducer from './shop/shop.reducer';
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
     'key': 'root', storage, whitelist: ['cart']
 }
+
 const rootReducer = combineReducers({
     user: userReducer,
     directory: directoryReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
